181c6c69c0dd1f4f4632875d457937c43c3f9f46
bhavesh <bhaveshjangid122@gmail.com>
2023-07-14 16:36:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
+import {CircularProgress, Slide, TextField} from "@mui/material";
+import { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+  const [cityName, setCityName] = useState("Jaipur")
+  const [inputText, setInputText] = useState("");
+   const [data, setData] = useState({});
+   const [error, setError] = useState(false);
+   const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=51f0b8c081dabb4a546ac5e102b4cef8&units=metric`
+    )
+    .then((res)=> {
+      if (res.status === 200) {
+        error && setError(false)
+        return res.json()
+      } else {
+        throw new Error("Something went wrong")
+      }
+    })
+    .then((data) => {
+     setData(data);
+    })
+    .catch(() => setError(true))
+    .finally(() => setLoading(false));
+  }, [cityName,error]);
+
+  const handelSearch = (e) => {
+    if (e.key === "Enter"){
+    setCityName(e.target.value);
+    setInputText("");
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+   <div className='bg_img'>
+    {!loading ? (
+        <>
+         <TextField 
+    variant="filled" 
+    label="Search location" 
+    className="input"
+    error={error} 
+    value={inputText}
+    onChange={(e) => setInputText(e.target.value)}
+    onKeyDown={handelSearch}
+    />
+    <h1 className="city">{data.name}</h1>
+    <div className="group">
+      <img  src={`http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`}
+        alt="" />
+      <h1>{data.weather[0].main}</h1>
+    </div>
+
+    <h1 className="temp">{data.main.temp.toFixed()} °C</h1>
+
+     <Slide direction="right" timeout={800} in={!loading}> 
+      <div className="box_container">
+        <div className="box">
+          <p>Humidity</p>
+          <h1>{data.main.humidity.toFixed()}%</h1>
+        </div>
+
+        <div className="box">
+          <p>Wind</p>
+          <h1>{data.wind.speed.toFixed()} km/h</h1>
+        </div>
+
+        <div className="box">
+          <p>Feels Like</p>
+          <h1>{data.main.feels_like.toFixed()} °C</h1>
+        </div>
+       </div>
+     </Slide> 
+ 
+        </>
+      ) : (
+        <CircularProgress/>
+      )
+    }
     </div>
   );
 }
 
 export default App;
+
